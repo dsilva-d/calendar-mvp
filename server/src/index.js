8c1799e1fa23054ede220723b5596acc81f0f866
dsilva-d <davejohnsonmyguy@gmail.com>
2026-03-29 18:38:40 -0400

    Failed to load calendar

--- server/src/index.js
+++ server/src/index.js
@@ -57,7 +57,7 @@ app.get("/", (req, res) => {
 app.get("/auth/google", (req, res) => {
   const authUrl = oauth2Client.generateAuthUrl({
     access_type: "offline",
-    prompt: "consent",
+    prompt: "select_account consent",
     scope: SCOPES,
   });
 
